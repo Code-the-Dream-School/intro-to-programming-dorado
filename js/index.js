c1abd0bdfc17d2fb90f0babd981567d32b2af1ab
Abebe <abetesso@gmail.com>
2022-11-25 12:15:28 -0800

    please add all files html/js/css

--- js/index.js
+++ js/index.js
@@ -5,15 +5,20 @@ console.log(thisYear); //2022
 const footer = document.querySelector("footer");
 const copyright = document.createElement("p");
 let mName = "Abebe";
-copyright.innerHTML = `${mName} ${thisYear}`;
+copyright.innerHTML = `&#169; ${mName} ${thisYear}`;
 footer.appendChild(copyright);
 const skills = ["HTML", "CSS", "JavaScript", "Cooking"];
 // console.log(skills);
- const skillSection = document.getElementById("skills"); // Do I need this line?
+const skillSection = document.getElementById("skills"); // Do I need this line?
 const skillsList = skillSection.querySelector("ul");
-    for(let i = 0; i < skills.length; i++) {
-        const skill = document.createElement("li");
-        skill.innerText = skills[i];
-        skillsList.appendChild(skill);
+for (let i = 0; i < skills.length; i++) {
+  const skill = document.createElement("li");
+  skill.innerText = skills[i];
+  skillsList.appendChild(skill);
 }
 console.log(skillsList);
+///exercise 4.3
+const messageForm = document.querySelector("leave_message");
+submit.addEventListener("click", function (e) {
+  alert("Hello");
+});
