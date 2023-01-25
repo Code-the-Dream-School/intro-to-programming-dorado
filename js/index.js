5aed39ccb7e64485113d3d6ab0ebef99309ce509
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-01-24 22:01:21 -0500

    adding Fetch API and styles

--- js/index.js
+++ js/index.js
@@ -1,75 +1,115 @@
 // footer info
 const today = new Date();
 const thisYear = today.getFullYear();
-const footer = document.querySelector('footer');
-const copyright = document.createElement('p');
-copyright.innerHTML = '© Anna Pestova ' + thisYear;
-footer.appendChild(copyright)
-//create list of skills 
-let skills = ['Computer proficiency', 'Communication skills', 'Problem-solving abilities', 'Organizational know-how'];
-const skillsSection = document.getElementById('skills');
+const footer = document.querySelector("footer");
+const copyright = document.createElement("p");
+copyright.innerHTML = "© Anna Pestova " + thisYear;
+footer.appendChild(copyright);
+//create list of skills
+let skills = [
+  "Computer proficiency",
+  "Communication skills",
+  "Problem-solving abilities",
+  "Organizational know-how",
+];
+const skillsSection = document.getElementById("skills");
 //create "ul" in #skills section
-const skillsList = skillsSection.querySelector('ul');
+const skillsList = skillsSection.querySelector("ul");
 //attach list ol skills to the #skills section
 for (let i = 0; i < skills.length; i++) {
-    let skill = document.createElement('li');
-    skill.innerHTML = skills[i];
-    skillsList.appendChild(skill)
+  let skill = document.createElement("li");
+  skill.innerHTML = skills[i];
+  skillsList.appendChild(skill);
 }
 //fill the #messages section
-let messageSection = document.getElementById('messages');
+let messageSection = document.getElementById("messages");
 //Hide the #messages section when the list is empty
-messageSection.style.display = 'none';
+messageSection.style.display = "none";
 //Handle message form submit
-const messageForm = document.querySelector("[name='leave_message']")
-messageForm.addEventListener('submit', function(event){
-event.preventDefault();
-let name1 = event.target.elements.name;
-let email1 = event.target.elements.email;
-let message1 = event.target.elements.message; 
-console.log(name1.value);
-console.log(email1.value);
-console.log(message1.value);
-// create message section to display submitted message
-let messageSection = document.getElementById('messages');
-let messageList = messageSection.querySelector('ul');
-let newMessage = document.createElement('li');
+const messageForm = document.querySelector("[name='leave_message']");
+messageForm.addEventListener(
+  "submit",
+  function (event) {
+    event.preventDefault();
+    let name1 = event.target.elements.name;
+    let email1 = event.target.elements.email;
+    let message1 = event.target.elements.message;
+    console.log(name1.value);
+    console.log(email1.value);
+    console.log(message1.value);
+    // create message section to display submitted message
+    let messageSection = document.getElementById("messages");
+    let messageList = messageSection.querySelector("ul");
+    let newMessage = document.createElement("li");
 
-newMessage.innerHTML = "<a href = 'mailto:"  +  email1.value + "'>" + name1.value + "</a> <span>" + message1.value + "</span>";
-// create the remove message  button
-let removeButton = document.createElement('button');
-removeButton.innerHTML = 'Remove';
-removeButton.type = 'button';
+    newMessage.innerHTML =
+      "<a href = 'mailto:" +
+      email1.value +
+      "'>" +
+      name1.value +
+      "</a> <span>" +
+      message1.value +
+      "</span>";
+    // create the remove message  button
+    let removeButton = document.createElement("button");
+    removeButton.innerHTML = "Remove";
+    removeButton.type = "button";
 
-removeButton.addEventListener('click', function(handles){
-var entry = removeButton.parentNode;
-entry.remove();
-});
-newMessage.appendChild(removeButton);
-messageList.appendChild(newMessage);
-messageSection.style.display = 'block';
-messageForm.reset();
-}, 
-false)
+    removeButton.addEventListener("click", function (handles) {
+      var entry = removeButton.parentNode;
+      entry.remove();
+    });
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    messageSection.style.display = "block";
+    messageForm.reset();
+  },
+  false
+);
 
-var githubRequest = new XMLHttpRequest();
-var repositories=[];
+// var githubRequest = new XMLHttpRequest();
+// var repositories=[];
 
-githubRequest.open('GET', 'https://api.github.com/users/AnnaPestova1/repos');
-githubRequest.send();
-githubRequest.addEventListener('load', function(){
-   repositories = JSON.parse(this.response); 
-    console.log(repositories)
-   
-let projectSection = document.getElementById('projects');
+// githubRequest.open('GET', 'https://api.github.com/users/AnnaPestova1/repos');
+// githubRequest.send();
+// githubRequest.addEventListener('load', function(){
+//    repositories = JSON.parse(this.response);
+//     console.log(repositories)
 
-let projectList = projectSection.querySelector('ul');
+// let projectSection = document.getElementById('projects');
 
-for (let l=0; l<repositories.length; l++) {
-    var project = document.createElement('li');
-    project.innerHTML = `<a href="${repositories[l].html_url}">${repositories[l].name}</a><span> Updated year: ${new Date(repositories[l].updated_at).getFullYear()}</span>`;
-    projectList.appendChild(project);
-}
-}
-)
+// let projectList = projectSection.querySelector('ul');
 
+// for (let l=0; l<repositories.length; l++) {
+//     var project = document.createElement('li');
+//     project.innerHTML = `<a href="${repositories[l].html_url}">${repositories[l].name}</a><span> Updated year: ${new Date(repositories[l].updated_at).getFullYear()}</span>`;
+//     projectList.appendChild(project);
+// }
+// }
+// )
+
+fetch("https://api.github.com/users/AnnaPestova1/repos", {
+  method: "GET",
+  mode: "cors",
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (repositories) {
+    var projectSection = document.getElementById("projects");
+    var projectList = projectSection.querySelector("ul");
+    // for (let l = 0; l < repositories.length; l++)
+    for (let l in repositories) {
+      var project = document.createElement("li");
+      let repository = repositories[l];
+      project.innerHTML = `<a href="${repository.html_url}">${
+        repository.name
+      }</a><span> Updated year: ${new Date(
+        repository.updated_at
+      ).getFullYear()}</span>`;
+      projectList.appendChild(project);
+    }
+  })
+  .catch(function (error) {
+    console.error("Error:", error);
+  });
