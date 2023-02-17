22c307ebfbcb8af425b64b8ea8f8938f290550eb
Rachel Westermann <112041289+rachellibrarian@users.noreply.github.com>
2023-02-16 23:31:22 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,51 +1,100 @@
-      // CHALLENGE 1. Insert Copyright Text in Footer
-  // --------------------------------------------------------------
-  const renderCopyright = () => {
-    const today = new Date()
-    const thisYear = today.getFullYear()
-
-    // DOM selection
-    const copyright = document.querySelector('#copyright')
-
-    // DOM manipulation (modify)
-    copyright.innerHTML = `&copy; Elliot Thompson ${thisYear}`
-  }
+   // Copyright Text in Footer (Javascript & The DOM)
+    const renderCopyright = () => {
+        const today = new Date()
+        const thisYear = today.getFullYear()
     
-    window.onload = copyDate;
-  }
-
-
-   // Lesson 4.2 Create List of Skills (Javascript & The DOM)
-  // --------------------------------------------------------------
-  const renderSkillsList = () => {
-    const skills = [
-      'JavaScript',
-      'HTML',
-      'CSS',
-      'Jira',
-      'Figma',
-      'Sketch',
-      'Adobe XD',
-      'MySQL',
-    ]
-
-    // DOM selection
-    const skillsSection = document.querySelector('#skills')
-    const skillsList = skillsSection.querySelector('ul')
-//Skills list
-    for (let i = 0; i < skills.length; i++) {
-      // DOM manipulation (create)
-      const skill = document.createElement('li')
-      skill.classList.add('tag')
-      skill.innerText = skills[i]
-
-      // DOM manipulation (modify)
+        // DOM selection
+        const copyright = document.querySelector('#copyright')
+    
+        // DOM manipulation (modify)
+        copyright.innerHTML = `&copy; Rachel Westermann ${thisYear}`
+      }
+  
+  
+  //Skills List
+    const renderSkillsList = () => {
+      const skills = [
+        'JavaScript',
+        'HTML',
+        'CSS',
+        'Project Management',
+        'Scrum',
+        'MySQL',
+      ]
+  
+  // DOM selection
+     const skillsSection = document.querySelector('#skills')
+     const skillsList = skillsSection.querySelector('ul')
+  //Skills list
+      for (let i = 0; i < skills.length; i++) {
+  //DOM manipulation (create)
+     const skill = document.createElement('li')
+        skill.classList.add('tag')
+       skill.innerText = skills[i]
+  
+  // DOM manipulation (modify)
       skillsList.appendChild(skill)
-    }
-  }
+      }
+   }
+     
+    
+  // Lesson 4.2 Create List of Skills (Javascript & The DOM)
+    // --------------------------------------------------------------
+    //const renderSkillsList = () => {
+      //const skills = [
+        //'JavaScript',
+        //'HTML',
+        //'CSS',
+        //'Jira',
+        //'Figma',
+        //'Sketch',
+        //'Adobe XD',
+        //'MySQL',
+      //]
+  
+  // DOM selection
+      //const skillsSection = document.querySelector('#skills')
+      //const skillsList = skillsSection.querySelector('ul')
+  //Skills list
+      //for (let i = 0; i < skills.length; i++) {
+        // DOM manipulation (create)
+       // const skill = document.createElement('li')
+        //skill.classList.add('tag')
+        //skill.innerText = skills[i]
+  
+        // DOM manipulation (modify)
+        //skillsList.appendChild(skill)
+     // }
+    //}
 
-   // Lesson 4.3 Handle Message Form Submit (HTML Forms and DOM Practice)
-  // --------------------------------------------------------------
+
+    //AJax 
+  
+  //const doAjax = async()=>{
+    //const response = await fetch('https://api.github.com/users/rachellibrarian/repos'(Method,'GET')= > 
+    //console.log("inside async",)= >,
+    //console.log(response),
+    //repositories = await response.json()
+    //const projectSection = document.getElementById('projects');
+    //Project Section Here//
+    //const projectList = projectSection.querySelector('ul');
+    //Add for loop to initiate over repositories//
+    //console.log("I am inside of the loop")
+    //console.log(repositories[index])
+      //for(let index=0; index < repositories.length; index++)
+      //const project=document.createElement('li');
+      //console.log(project);
+      //Set the inner text of the project variable to the array element//
+      //project.innertext=repositories[index].name
+      //Checnage the project element to the project list element//
+    //  projectList.appendChild(project),
+  //}
+  //doAjax()
+
+  
+  
+  
+  
   const BUTTON = {
     edit: 'edit',
     save: 'save',
@@ -187,7 +236,6 @@
         )
 
         const projectSection = document.querySelector('#projects')
-        console.log(projectSection)
         const projectList = projectSection.querySelector('ul')
 
         for (let repository of filteredData) {
@@ -197,11 +245,18 @@
         }
       })
   }
-
-  document.addEventListener('DOMContentLoaded', () => {
-    // renderCopyright() 
-    renderSkillsList()
-    renderMessageForm()
-    // renderProjectsWithXHR()
-    renderProjectsWithFetch()
-  })
+  
+  
+  
+  
+  
+  
+      document.addEventListener('DOMContentLoaded', () => {
+        renderCopyright()
+        renderSkillsList()
+        renderMessageForm()
+        // renderProjectsWithXHR()
+        renderProjectsWithFetch()
+      }
+      )
+      
